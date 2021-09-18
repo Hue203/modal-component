116d943888bf50ef1c09c4ceffb2826bd8937e2d
Hue203 <huenguyenmt.vn@gmail.com>
2021-09-18 22:27:10 +0700

    basicmodal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
 import ModalFullScreen from "./components/ModalFullScreen/ModalFullScreen";
+
 function App() {
   const [showModal, setShowModal] = useState(false);
+  const [submitLoading, setSubmitLoading] = useState(false);
+  const [modalText, setModalText] = useState("");
+  const handleSubmit = () => {
+    setModalText(
+      "Submitting Form ... The modal will be closed after two seconds"
+    );
+    setSubmitLoading(true);
+    setTimeout(() => {
+      setShowModal(false);
+      setSubmitLoading(false);
+    }, 2000);
+  };
+  const handleShow = () => {
+    setShowModal(true);
+  };
   return (
     <>
-      <div className="App">
-        <Button
-          onClick={() => setShowModal(true)}
-          variant="success"
-          style={{
-            margin: 30,
-            textAlign: "center",
-          }}
-        >
-          Open Modal FullScreen
-        </Button>
-        <ModalFullScreen show={showModal} onClose={() => setShowModal(false)} />
+      <div className="container">
+        <button className="buttonOpen" onClick={handleShow}>
+          <svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fas"
+            data-icon="arrow-alt-circle-down"
+            className="svg-inline--fa fa-arrow-alt-circle-down fa-w-10"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path
+              fill="currentColor"
+              d="M8 256C8 119 119 8 256 8s248 111 248 248-111 248-248 248S8 393 8 256zm292 116V256h70.9c10.7 0 16.1-13 8.5-20.5L264.5 121.2c-4.7-4.7-12.2-4.7-16.9 0l-115 114.3c-7.6 7.6-2.2 20.5 8.5 20.5H212v116c0 6.6 5.4 12 12 12h64c6.6 0 12-5.4 12-12z"
+            ></path>
+          </svg>
+          I'm Modal FullScreen
+        </button>
+        <ModalFullScreen
+          show={showModal}
+          onClose={() => setShowModal(false)}
+          submit={handleSubmit}
+          submitLoading={submitLoading}
+          modalText={modalText}
+        />
       </div>
     </>
   );
--- src/components/ModalFullScreen/ModalFullScreen.js
+++ src/components/ModalFullScreen/ModalFullScreen.js
@@ -1,34 +1,106 @@
 import React from "react";
-import { Button } from "react-bootstrap";
 import "./style.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ClipLoader } from "react-spinners";
 
-const ModalFullScreen = ({ show, onClose }) => {
+const ModalFullScreen = ({
+  show,
+  onClose,
+  submit,
+  submitLoading,
+  modalText,
+}) => {
   if (!show) {
     return null;
   }
+
   return (
-    <>
-      <div className="modal">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h4 className="modal-title">Modal title</h4>
+    <div className="container">
+      <div
+        className="modal-wrapper"
+        style={{
+          transform: show ? `translateY(0%)` : `translateY(-100%)`,
+          opacity: show ? "1" : "0",
+        }}
+      >
+        {submitLoading ? (
+          <>
+            <div className="text-center">
+              <ClipLoader color="#FFD700" size={150} loading={submitLoading} />
+              <div>{modalText}</div>
+            </div>
+          </>
+        ) : (
+          <div className="modal  animated animatedFadeInUp fadeInUp">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h4 className="modal-title">Modal Title</h4>
+                <span onClick={onClose} className="close-modal-btn">
+                  X
+                </span>
+              </div>
+              <div className="modal-body">
+                <h4>Modal Content</h4>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos,
+                  unde dolores quidem asperiores sed incidunt debitis porro
+                  totam corrupti, officia aspernatur qui sequi! Repellat aliquam
+                  quae eum qui fugiat similique.
+                </p>
+                <div className="modal-body-content">
+                  <form className="form">
+                    <label for="fname">First Name</label>
+                    <input
+                      type="text"
+                      placeholder="Your name here"
+                      name="firstname"
+                    />
+                    <label for="lname">Last Name</label>
+                    <input
+                      type="text"
+                      placeholder="Your name here"
+                      name="lastname"
+                    />
+                    <label for="email">Email</label>
+                    <input
+                      type="text"
+                      placeholder="Email Adress"
+                      name="email"
+                    />
+                    <p className="text-muted">
+                      We'll never share your email with anyone else.
+                    </p>
+                    <label for="country">Country</label>
+                    <select id="country" name="country">
+                      <option value="australia">Viet Nam</option>
+                      <option value="canada">Canada</option>
+                      <option value="usa">USA</option>
+                    </select>
+                    <input
+                      type="checkbox"
+                      id="infor"
+                      name="infor"
+                      value="infor"
+                    />
+                    <label for="infor"> Check me out</label>
+                  </form>
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button className="buttonClose" onClick={onClose}>
+                  {" "}
+                  Close
+                </button>
+                <button className="buttonClose" onClick={submit}>
+                  {" "}
+                  Submit
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="modal-body">
-            {" "}
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, unde
-            dolores quidem asperiores sed incidunt debitis porro totam corrupti,
-            officia aspernatur qui sequi! Repellat aliquam quae eum qui fugiat
-            similique.
-          </div>
-          <div className="modal-footer">
-            <Button variant="success" className="button" onClick={onClose}>
-              {" "}
-              Close
-            </Button>
-          </div>
-        </div>
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
